4c88ec4c7289ace478b950096454c6a59a216a09
Jose <jomaamga@outlook.es>
2025-05-24 18:52:33 +0200

    reverted last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import { Button } from "@/components/ui/button"
-
 function App() {
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me!</Button>
-      <Button>Click me!</Button>
-      <Button>Click me!</Button>
-      <Button>Click me!</Button>
-      <Button>Click me!</Button>
-    </div>
+    <>
+      CINEAPOLIS GARDEN
+    </>
   )
 }
 
