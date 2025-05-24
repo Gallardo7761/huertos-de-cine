db896a77113722f9a0c66642cc80878ee40731ad
Jose <jomaamga@outlook.es>
2025-05-24 03:14:10 +0200

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: "localhost",
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@/': '/src/',
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+          motion: ['framer-motion'],
+          axios: ['axios'],
+        }
+      }
+    }
+  }
+})
